ee78a8a9e35fcc7818f1c1a6b6439e7b38789551
eliezer Maciel <eliezeromaciel@gmail.com>
2025-07-31 12:50:26 -0300

    criacao do layout da page de cadastro de novo servico

---
+++ app/routes/servicos.tsx
@@ -0,0 +1,154 @@
+import { useState, useEffect } from "react";
+
+import {getConsumidores} from '../services/consumidores'
+
+export default function Servicos () {
+  
+  const [usuario, setUsuario] = useState <string[]> ([]); 
+  const [clientes, setClientes] = useState <string []> ([])  
+  const [clientesFiltrados, setClientesFiltrados] = useState <string []> ([])
+  const [inputNomeCliente, setInputNomeCliente] = useState ('')
+
+  const handleChangeInputNome =  (e: any) => { 
+    const valor: string = e.target.value // aqui eu consigo pegar o valor que o usuário digitou, não como VALUE do input, mas VALUE DO EVENTO ONCHANGE.
+    const valorLowerCase: string = valor.toLowerCase()
+    setInputNomeCliente(valor)    // então, dou este valor do onchange para o state 
+    const filtraClientes = clientes.filter ( ( elem: any ) => elem.nome.toLowerCase().includes(valorLowerCase))
+    setClientesFiltrados(filtraClientes)
+  }
+
+  const handleClienteSelected = (elem: any) => {
+    setInputNomeCliente(elem)
+    setClientesFiltrados([])
+  }
+  
+  const loadClientes = async () => {
+    const consumidores = await getConsumidores() // consumidores recebe .data {array de objetos}
+    setClientes(consumidores.data)
+  }
+
+  useEffect ( ()=> {
+    if (clientes.length === 0)
+      loadClientes()
+    console.log('executou useeffect ')
+  }, [clientes])
+
+
+    return (
+    <div className="container mt-4">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="border border-secondary p-4 rounded">
+            <h3 className="text-secondary mb-3">Novo Serviço</h3>
+
+            <form className="needs-validation" noValidate>
+
+              {/* cliente */}
+              <div className="mb-3">
+                <input 
+                  className="form-control" 
+                  type="text"
+                  name="cliente" 
+                  placeholder="Cliente"
+                  // maxLength="30"
+                  required 
+                  value={inputNomeCliente}
+                  onChange={handleChangeInputNome}
+                  onBlur={() => setTimeout(() => setClientesFiltrados([]), 200)} // delay com settimeout, sem ele, ao clicar no nome , antes de dar certo ele zera os clientesfiltrados (funcao acima )
+                />
+                <ul className='list-group position-absolute shadow'
+                  style={{ zIndex: 1000 }}    
+                >
+                  { clientesFiltrados.length>0 ?  (clientesFiltrados.map( (elem: any , index) => {
+                    return (
+                      <li 
+                        key={index}
+                        className='list-group-item list-group-item-action'
+                        style={{ cursor: 'pointer' }}
+                        onClick={ () => { 
+                          handleClienteSelected(elem.nome)
+                        }}
+                      >
+                        {elem.nome}
+                      </li>
+                      
+                    )
+                  })) : null}
+                </ul>
+              </div>
+
+              {/* veículo */}
+              <div className="mb-3">
+                <input 
+                  className="form-control" 
+                  type="text"
+                  name="veiculo" 
+                  placeholder="veículo"
+                  // maxLength="300"
+                  required 
+                />
+              </div> 
+               
+              {/* valor cobrado */}
+              <div className="mb-3">
+                <input 
+                  className="form-control" 
+                  type="number"
+                  name="valorCobrado" 
+                  placeholder="Valor cobrado"
+                  // maxLength="300"
+                  required 
+                />
+              </div>       
+
+              {/* quem receberá valor */}
+              <div className="mb-3">
+                <input 
+                  className="form-control" 
+                  type="text"
+                  name="quemRecebe" 
+                  placeholder="Quem receberá o valor"
+                  // maxLength="300"
+                  required 
+                />
+              </div> 
+
+              {/* endereço para retirada */}
+              <div className="mb-3">
+                <input 
+                  className="form-control" 
+                  type="text"
+                  name="enderecoRetirada" 
+                  placeholder="Endereço para retirada"
+                  // maxLength="300"
+                  required 
+                />
+              </div>   
+
+              {/* endereço de entrega */}
+              <div className="mb-3">
+                <input 
+                  className="form-control" 
+                  type="text"
+                  name="enderecoEntrega" 
+                  placeholder="Endereço de entrega"
+                  // maxLength="300"
+                  required 
+                />
+              </div>     
+
+              
+              <div className="d-grid">
+                <button type="submit" className="btn btn-primary">
+                  Criar
+                </button>
+              </div>
+              
+            </form>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    )
+}
